8eda14acf22c0005243403ade1ad36e49bf4f365
mhmd97z <mmd1997z@gmail.com>
2024-09-06 17:49:34 +0000

    updating cmars marabou query

--- marabou/cmars_query.py
+++ marabou/cmars_query.py
@@ -2,81 +2,48 @@
 from maraboupy import Marabou
 from maraboupy import MarabouNetworkONNX
 from maraboupy import MarabouCore, MarabouUtils
-
 import time
 
 
-def run_query(model_path, model_input_size, model_output_size, 
-    copy_one_argmax_candidate, copy_two_argmax_candidate, timeout):
-    options = Marabou.createOptions(verbosity = 0, timeoutInSeconds=timeout)
+def run_query(model_path, vnnlib_path):
+    options = Marabou.createOptions(verbosity=0, timeoutInSeconds=timeout)
     network = Marabou.read_onnx(model_path)
-
-    inputVars = network.inputVars[0][0]
-    outputVars = network.outputVars[0][0]
-
-    for i in range(model_input_size):
-        # print(f"setting bounds for input varaible {i}")
-        network.setLowerBound(inputVars[i], 0.0)
-        network.setUpperBound(inputVars[i], 1.0)
-
-    for i in range(model_input_size, 2*model_input_size):
-        # print(f"setting bounds for input varaible {i}")
-        network.setLowerBound(inputVars[i], 0.0001)
-        network.setUpperBound(inputVars[i], 0.0001)
-
-    for i in range(model_output_size):
-        if i == copy_one_argmax_candidate:
-            continue
-        # print("comparing output variable ", i)
-        equation = MarabouUtils.Equation(EquationType=MarabouCore.Equation.GE)
-        equation.addAddend(1, outputVars[copy_one_argmax_candidate])
-        equation.addAddend(-1, outputVars[i])
-        equation.setScalar(0)
-        network.addEquation(equation)
-
-    for i in range(model_output_size):
-        if i == copy_two_argmax_candidate:
-            continue
-        # print("comparing output variable ", i+model_output_size)
-        equation = MarabouUtils.Equation(EquationType=MarabouCore.Equation.GE)
-        equation.addAddend(1, outputVars[copy_two_argmax_candidate+model_output_size])
-        equation.addAddend(-1, outputVars[i+model_output_size])
-        equation.setScalar(0)
-        network.addEquation(equation)
-
-    exitCode, vals, stats = network.solve(options = options)
+    exitCode, _, _ = network.solve(options=options, propertyFilename=vnnlib_path)
 
     return exitCode
 
 if __name__ == "__main__":
-    layer_count = 2
+    layer_count = 1
     hidden_size = 32
     action_count = 15
-    model_path = f"../applications/cmars/models/conv2d_based_onnx/model_l{layer_count}_h{hidden_size}_a{action_count}.onnx"
     model_input_size = 19
     model_output_size = action_count
-    # indices = [6, 7, 8, 10, 13, 29, 30, 31, 33, 41, 42, 43, 45, 48, 52, 53, 54, 72, 75, 79, 80, 81, 83, 86, 87, 88, 90, 93]
+    indices = [50, 51, 52, 53, 54, 55]
     indices = None
     timeout = 3600
-    with open(f"../applications/cmars/output{action_count}_multiplexing_gain_lower00upper10.csv", "r") as file:
+    model_path = f"../applications/cmars/models/conv2d_based_onnx/model_l{layer_count}_h{hidden_size}_a{action_count}.onnx"
+    property_csv_file_path = f"../applications/cmars/output{action_count}_robustness_lower00upper10.csv"
+    
+    with open(property_csv_file_path, "r") as file:
         lines = file.readlines()
         result_dict = {}
         for idx, line in enumerate(lines):
+            line = line.strip()
+            if "vnnlib" not in line:
+                continue
+            
             if not (indices is None):
                 if not(idx in indices):
                     continue
-
-            argmax_a_b = line.split("/")[-1].split(".")[0].split("_")[1:]
-            argmax_a = int(argmax_a_b[0][1:])
-            argmax_b = int(argmax_a_b[1][1:])
-            print(f" ---- running query for property idx {idx} with argmax_a of {argmax_a} and argmax_b of {argmax_b}", flush=True)
+            
+            print(f" ---- running query for idx: {idx}, {line}", flush=True)
+            vnnlib_path = "../applications/cmars/" + line
             start_time = time.time()
-            exitCode = run_query(model_path, model_input_size, model_output_size, argmax_a, argmax_b, timeout)
+            exitCode = run_query(model_path, vnnlib_path)
             end_time = time.time()
 
             if not (exitCode in result_dict):
                 result_dict[exitCode] = []
-
             result_dict[exitCode].append(idx)
 
             print(f"execition time: {end_time - start_time}", flush=True)
